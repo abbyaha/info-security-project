1af1f57897c6a49c3051a10ba760d951e75cf2df
jewel.siren <jewel.siren@yahoo.com>
2016-03-27 15:32:02 -0400

    Working on layouts for the login and password settings.

---
+++ app/src/main/java/haseley/abby/info_security_password_creator_and_manager/CreatePassword.java
@@ -0,0 +1,13 @@
+package haseley.abby.info_security_password_creator_and_manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_password);
+    }
+}
