534ffd669da4e9a0adcd06a8f413efcbaec97887
DBellutta <bellutta.1@osu.edu>
2016-04-09 10:42:45 -0400

    Modified code to save files in Android.

--- app/src/main/java/haseley/abby/info_security_password_creator_and_manager/PasswordFile.java
+++ app/src/main/java/haseley/abby/info_security_password_creator_and_manager/PasswordFile.java
@@ -1,10 +1,9 @@
 package haseley.abby.info_security_password_creator_and_manager;
 
+import android.content.Context;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -34,10 +33,10 @@ public final class PasswordFile {
     // This method takes an ArrayList of data and writes it to the file with the specified name. It
     // requires a Cipher object initialized with the appropriate key in order to perform the
     // encryption.
-    private static void writeToFile(String fileName, Cipher cipher, ArrayList<PasswordEntry> data) throws IOException {
+    private static void writeToFile(Context context, String fileName, Cipher cipher, ArrayList<PasswordEntry> data) throws IOException {
         // Open the file buffer. Note that this is a CipherOutputStream wrapped in an
         // ObjectOutputStream, so everything will indeed be encrypted.
-        ObjectOutputStream output = new ObjectOutputStream(new CipherOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)), cipher));
+        ObjectOutputStream output = new ObjectOutputStream(new CipherOutputStream(new BufferedOutputStream(context.openFileOutput(fileName, Context.MODE_PRIVATE)), cipher));
 
         // Loop through all of the elements in the ArrayList and write them to the file.
         for (int i = 0; i < data.size(); i++) {
@@ -51,7 +50,7 @@ public final class PasswordFile {
 
     // This method takes an ArrayList of data, encrypts it using the specified key, and writes it to
     // the file with the specified name.
-    public static void encryptStore(String file, byte[] key, ArrayList<PasswordEntry> data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+    public static void encryptStore(Context context, String file, byte[] key, ArrayList<PasswordEntry> data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException {
         // Create the necessary Cipher object based on the key.
         SecretKeySpec sks = new SecretKeySpec(key, ALGORITHMTYPE);
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
@@ -59,24 +58,24 @@ public final class PasswordFile {
         cipher.init(Cipher.ENCRYPT_MODE, sks);
 
         // Write the data to the file.
-        writeToFile(file, cipher, data);
+        writeToFile(context, file, cipher, data);
     }
 
     // This is an alias for the other encryptStore() method that takes a String for the key instead
     // of an array of bytes.
-    public static void encryptStore(String file, String key, ArrayList<PasswordEntry> data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException {
-        encryptStore(file, key.getBytes(), data);
+    public static void encryptStore(Context context, String file, String key, ArrayList<PasswordEntry> data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+        encryptStore(context, file, key.getBytes(), data);
     }
 
     // This method reads all the PasswordEntry objects from the file with the specified name and
     // returns them in an ArrayList. It requires a Cipher object initialized with the appropriate
     // key to do the decryption.
-    private static ArrayList<PasswordEntry> readFromFile(String fileName, Cipher cipher) throws IOException, ClassNotFoundException {
+    private static ArrayList<PasswordEntry> readFromFile(Context context, String fileName, Cipher cipher) throws IOException, ClassNotFoundException {
         // Initialize an empty ArrayList to hold the data later on.
         ArrayList<PasswordEntry> data = new ArrayList<PasswordEntry>();
         // Open the file buffer. Note that this is a CipherInputStream wrapped in an
         // ObjectInputStream, so the decryption will happen automatically.
-        ObjectInputStream input = new ObjectInputStream(new CipherInputStream(new BufferedInputStream(new FileInputStream(fileName)), cipher));
+        ObjectInputStream input = new ObjectInputStream(new CipherInputStream(new BufferedInputStream(context.openFileInput(fileName)), cipher));
 
         try {
             // Continuously try to read an object from the file until the end of the file is reached
@@ -100,7 +99,7 @@ public final class PasswordFile {
 
     // This method reads all the PasswordEntry objects from the file with the specified name,
     // decrypts them using the specified key, and returns them in an ArrayList.
-    public static ArrayList<PasswordEntry> decryptStore(String file, byte[] key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, ClassNotFoundException {
+    public static ArrayList<PasswordEntry> decryptStore(Context context, String file, byte[] key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, ClassNotFoundException {
         // Create the necessary Cipher object based on the key.
         SecretKeySpec sks = new SecretKeySpec(key, ALGORITHMTYPE);
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
@@ -108,12 +107,12 @@ public final class PasswordFile {
         cipher.init(Cipher.DECRYPT_MODE, sks);
 
         // Read from the file and return the data.
-        return readFromFile(file, cipher);
+        return readFromFile(context, file, cipher);
     }
 
     // This is an alias for the other decryptStore() method that takes a String for the key instead
     // of an array of bytes.
-    public static ArrayList<PasswordEntry> decryptStore(String file, String key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, ClassNotFoundException {
-        return decryptStore(file, key.getBytes());
+    public static ArrayList<PasswordEntry> decryptStore(Context context, String file, String key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, ClassNotFoundException {
+        return decryptStore(context, file, key.getBytes());
     }
 }
